3baa75c7084d331d0f7870dedd92569c9b4f9301
mackenzie-turner <mackenzie-turner@users.noreply.github.com>
2015-04-12 17:54:52 -0300

    Update Layer.js

--- Editor/js/Layer.js
+++ Editor/js/Layer.js
@@ -1,22 +1,33 @@
+// Layer for maps
 function Layer()
 {
-	var tiles = null;
-	var n = null;
-	var w = null;
-	var h = null;
-	var sh = null;
-	var sw = null;
-	var dw = null;
-	var dh = null;
-	var tw = null;
+	var tiles = null;				// array of tiles on this layer
+	var n = null;					// String, name of this layer
+	var w = null;					// integer, width of this layer in tiles
+	var h = null;					// integer, height of the map in tiles
+	var sh = null;					// integer, height of source rectangles
+	var sw = null;					// integer, width of the source rectangles
+	var dw = null;					// integer, width of the destination rectangles
+	var dh = null;					// integer, height of the destination rectangles 
+	var tw = null;					// integer, width of the texturemap
 	
-	var vis = null;
+	var vis = null;					// boolean, true if the layer is visible
 	
-	var that = this;
-	var def = false;
+	var that = this;				// self reference; may remove later
+	var def = false;				// boolean, true if a create function has been called
 
-	var obj = [];
+	var obj = [];					// arrat, game objects on this layer 
 
+	// creates tiles from an array
+	// _n = string, layer name
+	// _r = array, integers to make into tiles
+	// _im = Image, texture map
+	// _w = width of the map in tiles
+	// _h = height of the map in tiles
+	// _sw = source rectangle width
+	// _sh = source rectangle height
+	// _dw = destination rectangle width
+	// _dh = destination re3ctangle height
 	this.createFromArray = function(_n, _r, _im, _w, _h, _sw, _sh, _dw, _dh)
 	{
 		tiles = [];
@@ -47,6 +58,17 @@ function Layer()
 		def = true;
 	}
 	
+	// creates tiles from an array
+	// _n = string, layer name
+	// _ctx = canvas context; not useful, remove at later date
+	// _im = Image, texture map
+	// _w = width of the map in tiles
+	// _h = height of the map in tiles
+	// _sw = source rectangle width
+	// _sh = source rectangle height
+	// _dw = destination rectangle width
+	// _dh = destination re3ctangle height
+	// _fx, _fy = coordinates for source rectangle to fill the map with
 	this.create = function(_n, _ctx, _im, _w, _h, _sw, _sh, _dw, _dh, _fx, _fy)
 	{
 		tiles = [];
@@ -73,9 +95,14 @@ function Layer()
 		def = true;
 	}
 	
+	// self explanitory
 	this.isVisible = function(){return(vis);}
 	this.setVisible = function(_v){vis = _v;}
 	
+	// _ctx = canvas context
+	// _im = Image, texture map
+	// _dx = leftmost index to draw
+	// _sy = topmost index to draw
 	this.draw = function(_ctx, _im, _dx, _dy)
 	{
 		var c = _ctx;
@@ -99,16 +126,20 @@ function Layer()
 		_ctx = c;
 	}
 	
+	// adds a game object to the object list
+	// _o = game object
 	this.addObject = function(_o)
 	{
 		obj.push(_o);
 	}
 	
+	// removes object at _i and returns it
 	this.removeObject = function(_i)
 	{
 		return(obj.splice(_i, 1));
 	}
 	
+	// sets the tile at index _x, _y to _s, _t
 	this.setTile = function(_x, _y, _s, _t)
 	{
 		tiles[_y][_x].setClip(_s, _t);
@@ -116,11 +147,13 @@ function Layer()
 		//con.message(tiles[0][0].getS()+", "+tiles[0][0].getT());
 	}
 	
+	// gets the name of the layer
 	this.getName = function()
 	{
 		return(n);
 	}
 	
+	// gets the tiles as an array of integers
 	this.getTileNumbers = function()
 	{
 		var tileNumbers = [];
@@ -135,6 +168,7 @@ function Layer()
 		return(tileNumbers);
 	}
 	
+	// gets the tiles as integers in a json formatted string
 	this.getJsonTileNumbers = function()
 	{
 		con.message("texture Width:"+tw);
