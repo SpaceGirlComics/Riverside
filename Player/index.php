6f43173173f6da37efd90eb01bc940cb923577ac
mackenzie-turner <mackenzie-turner@users.noreply.github.com>
2015-04-11 22:54:47 -0300

    Update index.php

--- Player/index.php
+++ Player/index.php
@@ -1,1 +1,31 @@
 
+<!DOCTYPE html>
+
+<?php 
+	$mInput = "oncontextmenu=\"return(false);\" onmousedown=\"onMouseDown(event, this)\" onmouseup=\"onMouseUp(event, this)\" onmouseout=\"onMouseOut(event, this)\" onmousemove=\"onMouseMove(event, this)\"";
+	$kInput = "onkeyup=\"onKeyUp(event)\" onkeydown=\"onKeyDown(event)\"";
+?>
+
+<html>
+	<head>
+		<title>
+			RPG POC
+		</title>
+		<link rel="stylesheet" type="text/css" href="css/game.css" />
+		<script type="text/javascript" src="js/common.js"></script>
+		<script type="text/javascript" src="js/Waypoint.js"></script>
+		<script type="text/javascript" src="js/Character.js"></script>
+		<script type="text/javascript" src="js/Tile.js"></script>
+		<script type="text/javascript" src="js/Layer.js"></script>
+		<script type="text/javascript" src="js/Map.js"></script>
+		<script type="text/javascript" src="js/Game.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+	</head>
+	<body <?php echo $kInput;?>>
+		<div class="screen">
+			<canvas id="can" width="800" height="550" <?php echo $mInput;?>>
+				A Modern Browser Is Required
+			</canvas>
+		</div>
+	</body>
+</html>
