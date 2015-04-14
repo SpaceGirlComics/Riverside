752de0afb533185d77996700bd75e416b6c7c1d1
SpaceGirlComics <SpaceGirlComics@users.noreply.github.com>
2015-04-13 22:25:20 -0300

    Update doLoad.php

--- Editor/doLoad.php
+++ Editor/doLoad.php
@@ -1,14 +1,16 @@
 <?php
+// server script. Loads a map as a string and sends it to the clients
 
+// check for the file name in the qstring
 if(isset($_POST['n']))
 {
 	$n = "maps/".$_POST['n'];
-	$f = fopen($n, "r");
+	$f = fopen($n, "r");				// opens the file
 	if($f)
 	{
-		$s = fread($f, filesize($n));
-		fclose($f);
-		echo $s;
+		$s = fread($f, filesize($n));		// read its contents
+		fclose($f);				// close the file
+		echo $s;				// echo the data to the client
 	}
 }
 
