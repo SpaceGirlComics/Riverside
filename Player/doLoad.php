61126af94edd94c436f0a95a8b3ba0d1ca0da5a4
SpaceGirlComics <SpaceGirlComics@users.noreply.github.com>
2015-04-13 22:44:15 -0300

    Update doLoad.php

--- Player/doLoad.php
+++ Player/doLoad.php
@@ -1,14 +1,15 @@
 <?php
+// Sends data from specified file to the client
 
-if(isset($_POST['n']))
+if(isset($_POST['n']))					// get the file name from the string
 {
 	$n = "maps/".$_POST['n'];
-	$f = fopen($n, "r");
+	$f = fopen($n, "r");				// open the specified file
 	if($f)
 	{
-		$s = fread($f, filesize($n));
-		fclose($f);
-		echo $s;
+		$s = fread($f, filesize($n));		// read the file contents
+		fclose($f);				// close th file
+		echo $s;				// echo contents back to client
 	}
 }
 
