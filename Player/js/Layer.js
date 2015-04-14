8e60e592da04a6848f74ba3be3a75c64c6722a3c
SpaceGirlComics <SpaceGirlComics@users.noreply.github.com>
2015-04-14 10:23:36 -0300

    Update Layer.js

--- Player/js/Layer.js
+++ Player/js/Layer.js
@@ -1,21 +1,32 @@
+// Layer for maps
 // © 2015 spacegirl.net
 // April 15, 2015 - initial submit
 
 function Layer()
 {
-	var name;
-	var width = 0;
-	var height = 0;
-	var sw;
-	var dw;
-	var sh;
-	var dh;
-	var tiles = [];
+	var name;				// string, name of the layer
+	var width = 0;				// int, width of the layer in tiles
+	var height = 0;				// int, height of the layer in tiles
+	var sw;					// source tile width
+	var dw;					// destination tile width
+	var sh;					// source tile height
+	var dh;					// destination tile height
+	var tiles = [];				// array of tile objects
 	
-	var dx = 0;
-	var dy = 0;
+	// used to skip drawing tiles that are not on sreen
+	var dx = 0;				// index of the leftmost tiles to appear
+	var dy = 0;				// index of the topmost tiles to appear
 	
-	
+	// defines the layer
+	// _n = string, the layer's name
+	// _tn = int, the tile value loaded from the map file
+	// _tw = int, the width of the texturemap being used
+	// _w = int, width of the layer in tiles
+	// _h = int, height of the layer in tiles
+	// _sw = int, source tile width
+	// _sh = int, source tile height
+	// _dw = int, destination tile width
+	// _dh = int, destination tile height
 	this.create = function(_n, _tn, _tw, _w, _h, _sw, _sh, _dw, _dh)
 	{
 		name = _n;
@@ -65,6 +76,10 @@ function Layer()
 		}
 	}
 	
+	// cyclic update
+	// _delta = float, delta time
+	// _dx = int, x translation
+	// _dy = int, y translation
 	this.update = function(_delta, _dx, _dy)
 	{
 		dx = iDivide(-_dx, dw);
