4fa49206e9ff7953f92e57473291e0ff611b2767
SpaceGirlComics <SpaceGirlComics@users.noreply.github.com>
2015-04-13 22:34:19 -0300

    Update doSaveAs.php

--- Editor/doSaveAs.php
+++ Editor/doSaveAs.php
@@ -1,20 +1,23 @@
 <?php
+// server script.
+
+// check if a file name is specified
 if(isset($_POST['n']))
 {
-	$fname = $_POST['n'];
-	$fdata = $_POST['m'];
-	$f = fopen("maps/".$fname, "w");
+	$fname = $_POST['n'];				// get the file name from the qstring
+	$fdata = $_POST['m'];				// get the map data from the qstring
+	$f = fopen("maps/".$fname, "w");		// open (create) the file
 	if($f)
 	{
-		if(fwrite($f, $fdata))
+		if(fwrite($f, $fdata))			// write data to the file
 		{
-			echo "File Saved As ".$fname;
+			echo "File Saved As ".$fname;	// echo success back to the client
 		}
 		else
 		{
-			echo "File Save Failed";
+			echo "File Save Failed";	// echo failure
 		}
-		fclose($f);
+		fclose($f);				// close the file
 	}
 }
 ?>
