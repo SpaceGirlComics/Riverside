14847666f20232cf68ba0c7dd292c66a0c14ebd3
SpaceGirlComics <SpaceGirlComics@users.noreply.github.com>
2015-04-23 11:16:14 -0300

    Update Console.js
    whitespace

--- Editor/js/Console.js
+++ Editor/js/Console.js
@@ -7,31 +7,36 @@ function Console(_c)
 {
 	var con = _c;
 	
-	this.message = function(_m)			// outputs a message (_m) with white text and begins a new line
+	// outputs a message (_m) with white text and begins a new line
+	this.message = function(_m)
 	{
 		con.innerHTML += _m + "<br />";
 		con.scrollTop = con.scrollHeight;
 	}
 
-	this.warning = function(_m)			// outputs a message (_m) with yellow text and begins a new line
+	// outputs a message (_m) with yellow text and begins a new line
+	this.warning = function(_m)
 	{
 		con.innerHTML += "<span class=\"warning\">" +_m + "</span><br />";
 		con.scrollTop = con.scrollHeight;
 	}
 
-	this.serious = function(_m)			// outputs a message (_m) with red text and begins a new line
+	// outputs a message (_m) with red text and begins a new line
+	this.serious = function(_m)
 	{
 		con.innerHTML += "<span class=\"serious\">" +_m + "</span><br />"; 
 		con.scrollTop = con.scrollHeight;
 	}
 
-	this.undec = function(_m)			// outputs a message without any added markup
+	// outputs a message without any added markup
+	this.undec = function(_m)
 	{
 		con.innerHTML += _m; 
 		con.scrollTop = con.scrollHeight;
 	}
 	
-	this.clear = function()				// clears all text from console
+	// clears all text from console
+	this.clear = function()
 	{
 		con.innerHTML = "";
 	}
