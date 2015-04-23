26e5bb34282a77a8c8997ac651cb8e3a87a16e5d
SpaceGirlComics <SpaceGirlComics@users.noreply.github.com>
2015-04-23 11:06:48 -0300

    Update Block.js
    Now uses Box

--- Editor/js/Block.js
+++ Editor/js/Block.js
@@ -1,28 +1,24 @@
 // collision block
 // © 2015 spacegirl.net
 // April 15, 2015 - initial submit
+
 function Block()
 {
-	var x;							// left coordinate
-	var y;							// top coordinate
-	var w;							// width of the block
-	var h;							// height of the block
-	
+	var b = new Box();
 	var s = true;						// true if box is selected
 	var v = true;						// true if visible
 	
 	var name;						// name of the object
 
-	this.create = function(_name, _x, _y, _tw, _th)		// defines or redefine new block
+	// defines or redefine new block
+	this.create = function(_name, _x, _y, _tw, _th)
 	{
 		name = _name;
-		x = _x;
-		y = _y;
-		w = _tw;
-		h = _th;
+		b.create(_x, _y, _tw, _th);
 	}
 	
-	this.draw = function(_ctx)				// draws the block if visible
+	// draws the block if visible
+	this.draw = function(_ctx)
 	{
 		_ctx.fillStyle = "#5555aa";
 		_ctx.strokeStyle = "#aaaaff";
@@ -32,7 +28,7 @@ function Block()
 			_ctx.strokeStyle = "#aaffaa";
 		}
 		_ctx.beginPath();
-		_ctx.rect(x, y, w, h);
+		_ctx.rect(b.getX(), b.getY(), b.getWidth(), b.getHeight());
 		_ctx.globalAlpha = 0.5;
 		_ctx.fill();
 		_ctx.globalAlpha = 1.0;
@@ -41,18 +37,18 @@ function Block()
 
 	// regular mutator and retrieval functions
 	
-	this.setX = function(_x){x = _x;}			// sets x position (left side) to the value of _x
-	this.setY = function(_y){y = _y;}			// sets y position (top side) to the vale of _y
-	this.setWidth = function(_w){w = _w;}			// sets width of the block to _w
-	this.setHeight = function(_h){h = _h;}			// sets height of the block to _h
+	this.setX = function(_x){b.setX(_x);}				// sets x position (left side) to the value of _x
+	this.setY = function(_y){b.setY(_y);}				// sets y position (top side) to the vale of _y
+	this.setWidth = function(_w){if(_w > 0){b.setWidth(_w);}}	// sets width of the block to _w
+	this.setHeight = function(_h){if(_h > 0){b.setHeight(_h);}}	// sets height of the block to _h
 	
-	this.getX = function(){return(x);}			// gets the x position (left side) of the block
-	this.getY = function(){return(y);}			// gets the y position (top side) of the block
-	this.getWidth = function(){return(w);}			// gets the width of the block
-	this.getHeight = function(){return(h);}			// gets the height of the block
+	this.getX = function(){return(b.getX());}			// gets the x position (left side) of the block
+	this.getY = function(){return(b.getY());}			// gets the y position (top side) of the block
+	this.getWidth = function(){return(b.getWidth());}		// gets the width of the block
+	this.getHeight = function(){return(b.getHeight());}		// gets the height of the block
 	
 	this.setSelected = function(_s){s = _s;}		// enables or disables selected state
-	this.isSelected = function(){return(s);}		// returns if the block is selected
+	this.isSelected = function(){return(s);}		// returns if the block is selected	
 	this.isVisible = function(){return(v);}			// returns if the block is visible
 	this.setVisible = function(_v){v = _v;}			// makes block visible or invisible
 }
